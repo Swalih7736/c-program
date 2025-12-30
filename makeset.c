7ecdc5513e1a9a04e5a998302869eebf4dc39a04
Ahame Swalih <ahamedswalih1111@gmail.com>
2025-12-30 12:29:20 +0530

    Add files via upload

--- makeset.c
+++ makeset.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-int parent[50];rank[50];
+int parent[50],rank[50];
 void make_set(int x)
 {
 	parent[x]=x;
 	rank[x]=0;
 
 }
-void find_set(int x)
+int find_set(int x)
 {
 	if(x != parent[x])
 		parent[x] = find_set(parent[x]);
-	return(parent [x]);
+	return parent [x];
 }
 
 void link(int x,int y)
@@ -34,7 +34,7 @@ void link(int x,int y)
 		}
 	}
 }
-void union(int x,int y)
+void union_set(int x,int y)
 {
 	link(find_set(x),  find_set(y));
 }
@@ -42,23 +42,24 @@ int main()
 {
 
 	make_set(0);
-	make_set(1;
+	make_set(1);
 	make_set(2);
 	make_set(3);
 	make_set(4);
+	make_set(5);
+	make_set(6);
 	
-	
-	union(0,1);
-	union(1,2);
-	union(3,4);
-	union(5,6);
-	union(4,5);
-	union(2,6);
+	union_set(0,1);
+	union_set(1,2);
+	union_set(3,4);
+	union_set(5,6);
+	union_set(4,5);
+	union_set(2,6);
 	
 	
 	
 	for(int i=0;i<7;i++)
 		printf("%d",parent[i]);
-	return(0);
+	return 0 ;
 	
 }
